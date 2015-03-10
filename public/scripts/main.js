3dc967957a2d0d3daeb2e4d2d3347ecc66a40c60
Darren Jack <darrencharlesjack79@gmail.com>
2015-03-10 09:10:34 -0600

    some map stuff

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -9,6 +9,40 @@ var initialize=function () {
             mapOptions);
       };
 
+var success = function (position) {
+  var mapcanvas = document.createElement('div');
+  mapcanvas.id = 'mapcontainer';
+  mapcanvas.style.height = '400px';
+  mapcanvas.style.width = '600px';
+
+  document.querySelector('article').appendChild(mapcanvas);
+
+  var coords = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  
+  var options = {
+    zoom: 15,
+    center: coords,
+    mapTypeControl: false,
+    navigationControlOptions: {
+    	style: google.maps.NavigationControlStyle.SMALL
+    },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("mapcontainer"), options);
+
+  var marker = new google.maps.Marker({
+      position: coords,
+      map: map,
+      title:"You are here!"
+  });
+}
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success);
+} else {
+  error('Geo Location is not supported');
+}
+
 
 $(document).on('ready', function(){
 	// Google maps initializer 
@@ -17,4 +51,5 @@ $(document).on('ready', function(){
 
 });
 
-// google.maps.event.addDomListener(window, 'load', initialize);
+
+
