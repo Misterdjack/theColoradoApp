ae2cd7115ac72d7aaf84766fb52c3b435b0cd4bb
Darren Jack <darrenjack79@yahoo.com>
2015-03-11 12:02:53 -0600

    added route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
+var mapController = require('./controllers/map.js');
 
 // Mongoose
 var mongoose = require('mongoose');
@@ -13,6 +14,9 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', indexController.index);
+
+// Api route
+app.get('/api/map', mapController.map);
 
 var server = app.listen(7194, function() {
 	console.log('Express server listening on port ' + server.address().port);
---
+++ controllers/map.js
@@ -0,0 +1,7 @@
+var mapController = {
+	map: function(req, res) {
+		res.render('map');
+	}
+};
+
+module.exports = mapController;
