c1fb5027e33b2bb89b31ba921e4ab02c9cee9b48
Darren Jack <darrencharlesjack79@gmail.com>
2015-03-18 17:28:46 -0600

    got stuff to stay above the footer

--- controllers/map.js
+++ controllers/map.js
@@ -9,6 +9,10 @@ var mapController = {
 	},
 
 	mapAdventures: function(req, res) {
+			// 
+			// Go to heroku, get into shell of mongoLabs, grab-key,
+			// then run command:  
+			// db.adventure.ensureIndex({coords: "2dshpere"})
 			// Adventure.find({type: req.params.type, coords: {$near: {lat: req.params.lat, lng: req.params.lng}, $maxDistance : 8046}}, function(err, docs){
 			Adventure.find({}, function(err, docs){
 			// Adventure.find({coords : { $near : [req.params.lon, req.params.lat], function(err, docs){
--- public/scripts/load.js
+++ public/scripts/load.js
@@ -1,30 +1,16 @@
 
-var mapView = function(e){
-  e.preventDefault();
 
-  $('#viewMap-modal').modal('show');
-
-  var adventureElement = $(this).closest('.adventure');
-  var targetId = adventureElement.attr('data-id');
-
-  $.get($(this).attr('href'), function(data){
+$(window).load(function(){
     
-  });
-};
-
-
-// Initialize the event listeners
-$(document).on('ready', function(){
-
-  // Bootstrap Material Design
-  $.material.init();
-
-  // $('#new-adventure').on('submit', onAdventureSubmit);
-
-  // Handle view clicks
-  $(document).on('click', '.view', adventureView);
-
-  // $(document).on('click', '.viewLocation', mapView);
-
+    $('#load-modal').modal('show');
 
 });
+
+// Initialize the event listeners
+// $(document).on('ready', function(){
+
+// 	$('#load-modal').modal('show');
+
+
+
+// });
