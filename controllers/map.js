a6b5bd8e475c3c152fef2f5024b6326df81b4362
Darren Jack <darrencharlesjack79@gmail.com>
2015-08-04 17:21:54 -0600

    updated packages and some controller logic

--- controllers/map.js
+++ controllers/map.js
@@ -14,8 +14,7 @@ var mapController = {
 
 	mapAdventures: function(req, res) {
 			// 
-			// Go to heroku, get into shell of mongoLabs, grab-key,
-			// then run command:  
+			// Go to heroku, get into shell of mongoLabs, grab-key, then run command:  
 			// db.adventure.ensureIndex({coords: "2dshpere"})
 			// Adventure.find({type: req.params.type, coords: {$near: {lat: req.params.lat, lng: req.params.lng}, $maxDistance : 8046}}, function(err, docs){
 			Adventure.find({}, function(err, docs){
